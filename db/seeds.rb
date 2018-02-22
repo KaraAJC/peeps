403874403c5c7ba9ed34216ec23c1b83efacf04a
KaraAJC <KaraAJC@gmail.com>
2018-02-22 14:39:39 -0600

    updated seed with person relation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,10 @@
 
 10.times do
 	Person.create!(name: Faker::RuPaul.unique.queen, age: rand(10...90))
-	Pet.create!(name: Faker::Zelda.unique.character, breed: Faker::Dog.breed, age: rand(1...10), person_id: rand(1...10) )
-
 	party_time = Faker::Time.forward(23, :night)
 	Party.create!(title: Faker::BossaNova.song, start_time: party_time, end_time: (party_time + 3600*2), pets_invited: Faker::Boolean.boolean )
+end
+
+10.times do
+	Pet.create!(name: Faker::Zelda.unique.character, breed: Faker::Dog.breed, age: rand(1...10), person: Person.find(rand(1...10)) )
 end
