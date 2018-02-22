1f12e926441df8dc08ab6a3a7c025b2a6e86d820
KaraAJC <KaraAJC@gmail.com>
2018-02-22 14:22:16 -0600

    seeds app with people, pets and parties

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+10.times do
+	Person.create!(name: Faker::RuPaul.unique.queen, age: rand(10...90))
+	Pet.create!(name: Faker::Zelda.unique.character, breed: Faker::Dog.breed, age: rand(1...10), person_id: rand(1...10) )
+
+	party_time = Faker::Time.forward(23, :night)
+	Party.create!(title: Faker::BossaNova.song, start_time: party_time, end_time: (party_time + 3600*2), pets_invited: Faker::Boolean.boolean )
+end
